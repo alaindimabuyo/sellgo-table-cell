dd94491849b1edcfed1d2871f8cb16defc065f8b
alainquanta <alain@quanta.com.ph>
2020-02-09 20:40:36 +0000

    Updates

---
+++ precache-manifest.8fb418a0163d9a3003a47965e2bfa4c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d79d1eeca99a8a2fcd08dea04d877c6b",
+    "url": "/sellgo-table-cell/index.html"
+  },
+  {
+    "revision": "7d637436a17d9ff91eee",
+    "url": "/sellgo-table-cell/static/css/main.66fdebf8.chunk.css"
+  },
+  {
+    "revision": "dbc5ab9ed7253fb7d908",
+    "url": "/sellgo-table-cell/static/js/2.e4374981.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/sellgo-table-cell/static/js/2.e4374981.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d637436a17d9ff91eee",
+    "url": "/sellgo-table-cell/static/js/main.06d7cf81.chunk.js"
+  },
+  {
+    "revision": "3b75b6d866df8c5d9697",
+    "url": "/sellgo-table-cell/static/js/runtime-main.26015171.js"
+  },
+  {
+    "revision": "c11ac77dcf6ff64d88670d3fb1ee67a8",
+    "url": "/sellgo-table-cell/static/media/img02.c11ac77d.jpeg"
+  }
+]);
